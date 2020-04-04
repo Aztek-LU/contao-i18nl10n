4770b419c6a50b3f72309f83f9595c13dd7809af
LupusVII <jln.thirion@gmail.com>
2020-04-04 18:12:34 +0200

    Add field in wizard href

--- src/Callback/WizardFieldCallback.php
+++ src/Callback/WizardFieldCallback.php
@@ -12,7 +12,6 @@ class WizardFieldCallback {
 	 */
 	public function addI18nl10nFields($dc)
 	{
-		
-		return ' <a href="contao/main.php?do=' . \Input::get('do') . '&amp;table=tl_i18nl10n_translation&amp;id=' . $dc->id . '&amp;popup=1&amp;nb=1&amp;rt=' . REQUEST_TOKEN . '" title="' . \Contao\StringUtil::specialchars($title) . '" onclick="Backend.openModalIframe({\'title\':\'' . \Contao\StringUtil::specialchars(str_replace("'", "\\'", $title)) . '\',\'url\':this.href});return false">' . \Contao\Image::getHtml('bundles/verstaerkeri18nl10n/img/i18nl10n.png', $title) . '</a>';
+		return ' <a href="contao/main.php?do=' . \Input::get('do') . '&amp;table=tl_i18nl10n_translation&amp;id=' . $dc->id . '&amp;popup=1&amp;nb=1&amp;field=' . $dc->field  . '&amp;rt=' . REQUEST_TOKEN . '" title="' . \Contao\StringUtil::specialchars($title) . '" onclick="Backend.openModalIframe({\'title\':\'' . \Contao\StringUtil::specialchars(str_replace("'", "\\'", $title)) . '\',\'url\':this.href});return false">' . \Contao\Image::getHtml('bundles/verstaerkeri18nl10n/img/i18nl10n.png', $title) . '</a>';
 	}
 }
